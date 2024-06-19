a629747166a6bf9b38a02648e96c754c05efbfbf
Quốc Việt <thaibinhquocviet2002@gmail.com>
2024-06-19 10:02:59 +0700

    update show blog model with category

--- schemas/show_blog.py
+++ schemas/show_blog.py
@@ -3,12 +3,22 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class BlogCategory(BaseModel):
+    id: int
+    name: str
+    description: str | None
+
+    class Config:
+        orm_mode = True
+
+
 class ShowBlog(BaseModel):
     id: int
     title: str
     slug: str
     content: Optional[str]
     created_at: date
+    categories: list[BlogCategory]
 
     class Config:
         orm_mode = True
